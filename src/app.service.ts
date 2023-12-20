dec8a17987cfa229f384d3216734bf9dde1320ae
Ahren <ahren@meetingpal.co.za>
2023-12-20 09:42:01 +0200

    more nest config things

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello, getting start with Nest!';
   }
 }
